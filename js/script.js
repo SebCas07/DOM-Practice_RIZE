502a2190cba3cf19de8ab1369782b822c2ce72d6
SebCas07 <tiancas07@gmail.com>
2026-03-17 22:35:29 -0500

    Completed javaScript functions that made website more functional by: Having the button be highlighted after clicking on it and removing the highlight feature when a different button is selected; Having a text output appear and change based on the button that was selected.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log("script.js loaded");
+
+let title = document.getElementById("page-title"); 
+let intructions = document.getElementById("mood-instructions"); 
+
+console.log(title); 
+console.log(intructions); 
+
+title.textContent = "How Are You Feeling Today?"; 
+intructions.textContent = "Click a mood below to begin."; 
+
+let buttons = document.querySelectorAll("#mood-buttons button"); 
+console.log(buttons); 
+
+buttons.forEach(function(button){
+    button.addEventListener("click", function(){
+
+        buttons.forEach(function(btn){ 
+            btn.classList.remove("selected"); 
+        }); 
+
+        button.classList.add("selected"); 
+        
+        let emoji = button.textContent; 
+        let output = document.getElementById("output-message"); 
+        output.textContent = `You are feeling ${emoji} today!`; 
+    })
+})
+
